258a80a516664b01445c6b636bad7165dff66047
AllenKo5 <69921088+AllenKo5@users.noreply.github.com>
2022-09-08 10:24:18 -0400

    fixed use errors

--- src/htmlResponses.js
+++ src/htmlResponses.js
@@ -4,6 +4,12 @@ const index = fs.readFileSync(`${__dirname}/../client/client.html`);
 const client2 = fs.readFileSync(`${__dirname}/../client/client2.html`);
 const client3 = fs.readFileSync(`${__dirname}/../client/client3.html`);
 
+const getHtml = (request, response, file) => {
+  response.writeHead(200, { 'Content-Type': 'text/html' });
+  response.write(file);
+  response.end();
+};
+
 const getIndex = (request, response) => {
   getHtml(request, response, index);
 };
@@ -16,14 +22,8 @@ const getClient3 = (request, response) => {
   getHtml(request, response, client3);
 };
 
-const getHtml = (request, response, file) => {
-  response.writeHead(200, { 'Content-Type': 'text/html' });
-  response.write(file);
-  response.end();
-}
-
 module.exports = {
-    getIndex,
-    getClient2,
-    getClient3
+  getIndex,
+  getClient2,
+  getClient3,
 };
--- src/mediaResponses.js
+++ src/mediaResponses.js
@@ -1,18 +1,6 @@
 const fs = require('fs');
 const path = require('path');
 
-const getParty = (request, response) => {
-  loadFile(request, response, '../client/party.mp4', 'video/mp4');
-};
-
-const getBling = (request, response) => {
-  loadFile(request, response, '../client/bling.mp3', 'audio/mpeg');
-};
-
-const getBird = (request, response) => {
-  loadFile(request, response, '../client/bird.mp4', 'video/mp4');
-};
-
 const loadFile = (request, response, filePath, contentType) => {
   const file = path.resolve(__dirname, filePath);
 
@@ -64,8 +52,20 @@ const loadFile = (request, response, filePath, contentType) => {
   });
 };
 
+const getParty = (request, response) => {
+  loadFile(request, response, '../client/party.mp4', 'video/mp4');
+};
+
+const getBling = (request, response) => {
+  loadFile(request, response, '../client/bling.mp3', 'audio/mpeg');
+};
+
+const getBird = (request, response) => {
+  loadFile(request, response, '../client/bird.mp4', 'video/mp4');
+};
+
 module.exports = {
-    getParty,
-    getBling,
-    getBird
+  getParty,
+  getBling,
+  getBird,
 };
